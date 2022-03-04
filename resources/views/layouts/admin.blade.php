d3dda909e70c6346396023f351a96028156441e8
lorenzosavoia <lorenzo.savoia97@gmail.com>
2022-03-05 00:41:45 +0100

    cute layout

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="app">
+    @include('partials.header')
+
+    <main class="py-4">
+    <div class="container-fluid">
+    <div class="row">
+        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+            <div class="position-sticky pt-3">
+                 <ul class="nav flex-column">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">
+                        <i class="bi bi-house-fill"></i>
+                         Dashboard
+                    </a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="bi bi-archive"></i>
+                        Posts
+                    </a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="bi bi-folder2"></i>
+                         Categories
+                    </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="col">
+            @yield('content')
+        </div>
+    </div>
+    </div>       
+    </main>
+</div>
+</body>
+</html>
