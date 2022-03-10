bb348336c40300378776b2fd60630c0916b87f92
lorenzosavoia <lorenzo.savoia97@gmail.com>
2022-03-10 17:22:53 +0100

    layout for vue

---
+++ resources/views/guest/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div id="app">
+    <div class="container">
+        <div class="row justify-content-center">
+            <h1>Ciao sono la home da modificare</h1>
+        </div>
+    </div>
+</div>
+@endsection
